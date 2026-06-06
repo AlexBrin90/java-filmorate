676648dea28b4c5fb12bfb4f4303abfac6caf781
E.Alexeev <hasala90@yandex.ru>
2026-06-07 01:52:55 +0300

    v0 - init (without unit-tests)

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,27 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+import java.time.LocalDate;
+
+
+@Data
 public class Film {
+
+    private Long id;
+
+    @NotNull
+    @NotBlank(message = "WARN: Название фильма не может быть пустым")
+    private String name;
+
+    @Size(max = 200, message = "WARN: Описание не должно превышать 200 символов")
+    @NotBlank(message = "WARN: Поле не может содержать только пробелы или быть пустым")
+    private String description;
+
+    private LocalDate releaseDate;
+
+    private int duration;
 }
